f45056a06a472d272133dec453da95c7441911ea
Philip Chang <sgnoohc@gmail.com>
2020-12-02 13:54:06 -0800

    Clean up some script locations

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,1 +1,2 @@
-./doAnalysis -t newTree -o hists/output_vbshww.root -i samples/Result_2017VBSWWH/sig_lep_results.root -d
+./doAnalysis -i /hadoop/cms/store/group/snt/nanoaod/mc/RunIIFall17NanoAODv7/TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/NANOAODSIM/PU2017_12Apr2018_Nano02Apr2020_102X_mc2017_realistic_v8-v1/20000/91DA3343-F0A0-4345-A7E5-CE8CF5A7889D.root -t Events -o outputs/output_ttw_15.root -s 0.000076266 -j 16 -I 15 -d -n -1
+
