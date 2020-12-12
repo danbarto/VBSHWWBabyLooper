e887b7bad867558821c3f51df65fe57a6823d3b4
Philip Chang <sgnoohc@gmail.com>
2020-12-12 10:34:11 -0800

    Moving analysis to use NanoAOD skimmer via nanoAOD-tools postprocessor.
    - The NanoAODSkimmer contains the condor submission code to skim
    - The outputs will be on hadoop
    - scripts/(run,exec,hadd)_v2.sh are added that runs on skimmed output

---
+++ scripts/hadd_v2.sh
@@ -0,0 +1,55 @@
+# Help
+usage()
+{
+  echo "ERROR - Usage:"
+  echo
+  echo "      sh $(basename $0) STUDYNAME"
+  echo
+  exit
+}
+
+if [ -z ${1} ]; then usage; fi
+STUDY=${1}
+
+YEARS="2016 2017 2018 Run2"
+
+SAMPLES="TTJets_DiLept \
+TTJets_SingLept_FromT \
+TTJets_SingLept_FromTbar \
+TTWJetsToLNu \
+TTZToLLNuNu \
+WZTo3LNu \
+WpWpJJ_EWK-QCD \
+ttHTobb"
+
+rm .jobs.txt
+
+for YEAR in ${YEARS}; do
+
+    HADDDIR=hadds/${STUDY}_${YEAR}
+
+    if [[ ${YEAR} == *"Run2"* ]]; then
+        HISTDIR="hists/${STUDY}_*"
+    else
+        HISTDIR=hists/${STUDY}_${YEAR}
+    fi
+
+    mkdir -p ${HADDDIR}
+    rm -rf ${HADDDIR}/*.root
+
+    echo "hadd -f ${HADDDIR}/ssww.root ${HISTDIR}/WpWp*.root > ${HADDDIR}/ssww.hadd.log 2>&1" >> .jobs.txt
+    echo "hadd -f ${HADDDIR}/tt1l.root ${HISTDIR}/TTJets_Sing*.root > ${HADDDIR}/tt1l.hadd.log 2>&1" >> .jobs.txt
+    echo "hadd -f ${HADDDIR}/tt2l.root ${HISTDIR}/TTJets_Di*.root > ${HADDDIR}/tt2l.hadd.log 2>&1" >> .jobs.txt
+    echo "hadd -f ${HADDDIR}/tth.root ${HISTDIR}/ttHTobb*.root > ${HADDDIR}/tth.hadd.log 2>&1" >> .jobs.txt
+    echo "hadd -f ${HADDDIR}/ttw.root ${HISTDIR}/TTW*.root > ${HADDDIR}/ttw.hadd.log 2>&1" >> .jobs.txt
+    echo "hadd -f ${HADDDIR}/ttz.root ${HISTDIR}/TTZ*.root > ${HADDDIR}/ttz.hadd.log 2>&1" >> .jobs.txt
+    echo "hadd -f ${HADDDIR}/wz.root ${HISTDIR}/WZ*.root > ${HADDDIR}/wz.hadd.log 2>&1" >> .jobs.txt
+    echo "hadd -f ${HADDDIR}/vbshww.root ${HISTDIR}/TTW*.root > ${HADDDIR}/vbshww.hadd.log 2>&1" >> .jobs.txt
+    #TODO TODO ADD SIGNAL
+    # echo "hadd -f ${HADDDIR}/vbshww.root ${HISTDIR}/VBSHWW*.root > ${HADDDIR}/vbshww.hadd.log 2>&1" >> .jobs.txt
+
+done
+
+echo "Launching hadd jobs in parallel...."
+xargs.sh .jobs.txt
+echo "Done!"
