1a1de51516a61707b0c7e771f35cebdf7683391a
Philip Chang <sgnoohc@gmail.com>
2021-03-16 16:06:19 -0700

    update

---
+++ scripts/setup_higgs_combine_tool.sh
@@ -0,0 +1,10 @@
+# Following taken from here: https://cms-analysis.github.io/HiggsAnalysis-CombinedLimit/
+
+cmsrel CMSSW_10_2_13
+cd CMSSW_10_2_13/src
+cmsenv
+git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
+cd HiggsAnalysis/CombinedLimit
+git fetch origin
+git checkout v8.2.0
+scramv1 b clean; scramv1 b # always make a clean build
